38fa54ada0cfe21c1f54458f53b0f4188be2f3d7
yarastqt <yarastqt@gmail.com>
2021-03-16 15:10:46 +0300

    fix: refactor template src

--- template/src/App.test.tsx
+++ template/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { App } from './App';
+
+describe('App', () => {
+  test('should render app with button', () => {
+    render(<App />);
+    const button = screen.getByText(/Button/i);
+    expect(button).toBeInTheDocument();
+  });
 });
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { configureRootTheme } from '@yandex/ui/Theme'
+import { theme } from '@yandex/ui/Theme/presets/default'
 
-ReactDOM.render(
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+configureRootTheme({ theme });
+
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
