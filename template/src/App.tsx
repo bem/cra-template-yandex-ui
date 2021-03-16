b812bebc0c34f5f056ba358b008da034d2f7a08b
yarastqt <yarastqt@gmail.com>
2021-03-16 15:10:46 +0300

    fix: refactor template src

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FC } from 'react';
+import { Button } from '@yandex/ui/Button/desktop/bundle';
+
 import './App.css';
 
-function App() {
+export const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button view="default" size="m">
+        Button
+      </Button>
     </div>
   );
-}
-
-export default App;
+};
